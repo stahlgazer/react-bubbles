1d55f4df48f6d271a8aaae6d9f767004c98f2045
gavin stahl <stahlgavin@gmail.com>
2020-01-31 10:59:14 -0800

    mvp functionality is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Link to="/" className="nav-links">
+          Login
+        </Link>
+        <Link to="/bubbles" className="nav-links">
+          Bubbles
+        </Link>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/bubbles" component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = event => {
+    setUserData({ ...userData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", userData)
+      .then(response => {
+        console.log("Successful Login", response);
+        localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => {
+        console.log(err);
+        console.log("Login failed for:", userData.username, userData.password);
+      });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form name="login" className="login">
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button color="info" name="submit" onClick={handleSubmit}>
+          Log in
+        </button>
+      </form>
     </>
   );
 };
